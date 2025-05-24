6b46dd749f816c38b0de5409b6b81aadb144f65a
RishikaBothra <rshkbothra@gmail.com>
2025-05-24 23:41:41 +0530

    updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,39 @@
-import { Request, Response, NextFunction } from "express";
+import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
-export const userMiddleware = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "Unauthorized: Token missing" });
+declare global {
+  namespace Express {
+    interface Request {
+      userId?: string;
+    }
   }
+}
 
-  const token = authHeader.split(" ")[1];
-
+export const userMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+  const header = req.headers["authorization"];
+  (header)
+  const JWT_SECRET = process.env.JWT_SECRET;
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
-    req.userId = (decoded as any).userId;
-    next();
-  } catch (err) {
-    return res.status(403).json({ message: "Invalid token" });
+    ("opop")
+    const decoder = jwt.verify(header as string, JWT_SECRET as string);
+    if (decoder) {
+      ("jkl")
+      req.userId = (decoder as jwt.JwtPayload).userId;
+      next();
+
+    }
+    else {
+      res.status(401).json({
+        message: "Unauthorized User"
+      });
+    }
   }
-};
+  catch (error) {
+    res.status(401).json({
+      message: "Unauthorized User"
+    });
+  }
+}
+
 
