cf15448ea857bf1016b5f22894042fb04e207910
RishikaBothra <rshkbothra@gmail.com>
2025-06-06 23:26:14 +0530

    .

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+function App() {
+
+  return (
+    // <div className="">
+    //   <Button variant="secondary" text="Share Brain"/>
+    //   <Button variant="primary" text="Add Content"/>
+    // </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/ui/button.ts
@@ -0,0 +1,30 @@
+import type { ReactElement } from "react";
+
+interface buttonprops{
+    variant :"primary"|"secondary";
+    size: "sm"|"md"|"lg";
+    text:string;
+    starticon?:ReactElement;
+    endicon?:ReactElement;
+    onclick:() =>void
+    //onClick is a function that takes no parameters and returns nothing.
+
+}
+
+const variantStyles = {
+    "primary":"bg-blue-600 text-white",
+    "secondary":"bg-blue-400 text-white"
+}
+
+const defaultStyles = " px-4 py-2 rounded-md";
+
+
+export const Button = (props:buttonprops) => {
+    return (
+        <button className={variantStyles[props.variant]+ defaultStyles}>
+            {props.text}
+        </button>
+    );
+}
+
+<Button variant="primary" size="md" onClick={() => {}} text="Share" />
